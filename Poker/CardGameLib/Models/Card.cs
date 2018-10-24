964b5f56f59647d80f707eb8d24913547005c5bc
Michael Zopff <michael.zopff@gmail.com>
2018-10-24 14:01:32 -0600

    Begin Framework

---
+++ Poker/CardGameLib/Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameLib.Models {
+    public class Card {
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        
+        public Card(Suit suit, Face face) {
+            Suit = suit;
+            Face = face;
+        }
+    }
+    public enum Suit {
+        HEARTS,
+        DIAMONDS,
+        CLUBS,
+        SPADES
+    }
+    public enum Face {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        JOKER
+    }
+}
---
+++ Poker/CardGameLib/Models/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameLib.Models {
+    class Deck : List<Card> {
+        public Deck(bool withJoker = false) {
+            foreach (Suit suit in Enum.GetValues(typeof(Suit))) {
+                foreach (Face face in Enum.GetValues(typeof(Face))) {
+                    this.Add(new Card(suit, face));
+                }
+            }
+        }
+    }
+}
