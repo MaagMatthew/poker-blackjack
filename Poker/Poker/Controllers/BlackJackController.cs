2fea2b6bacb9556acbf17fcfdf1a9476ea93c00d
Matthew Maag <meh.kirby@gmail.com>
2018-10-24 14:26:43 -0600

    Started implementing a Player Class

---
+++ Poker/Poker/Controllers/BlackJackController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGameLib.Models;
+
+
+namespace Poker.Controllers
+{
+    class BlackJackController
+    {
+        Deck deck = new Deck();
+
+
+        public void NewHand()
+        {
+            
+        }
+
+    }
+}
