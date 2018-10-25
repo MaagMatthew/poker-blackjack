53926ef8e9ebda7fa668045bf536b52160dd051c
AliceMcClain <36777803+AliceMcClain@users.noreply.github.com>
2018-10-25 14:15:50 -0600

    Main Window Setup

--- Poker/Poker/SetUpWindow.xaml.cs
+++ Poker/Poker/SetUpWindow.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Poker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SetUpWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SetUpWindow : Window
     {
-        public MainWindow()
+        public SetUpWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
