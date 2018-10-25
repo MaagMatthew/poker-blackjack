1fb3694a7ae93e8093ab99f2172daf5c2a078ea6
Matthew Maag <meh.kirby@gmail.com>
2018-10-25 14:09:33 -0600

    Added blackjack dictionary to be able to find the value of the hand more easily. Also Added a House Class and a PokerController

---
+++ Poker/CardGameLib/Models/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameLib.Models
+{
+    public class House
+    {
+        public Deck HouseHand { get; set; }
+
+        public House()
+        {
+            HouseHand = new Deck(true);
+        }
+    }
+}
