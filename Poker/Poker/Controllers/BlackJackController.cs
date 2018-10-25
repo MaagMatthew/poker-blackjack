bb9dd2cb909c9bf6836986489a38bdd6cc2d05f9
Dean Pesek <DPesek@student.neumont.edu>
2018-10-25 09:35:02 -0600

    Adding A EndOfRound Method and Payout method to the BlackJackController.cs

--- Poker/Poker/Controllers/BlackJackController.cs
+++ Poker/Poker/Controllers/BlackJackController.cs
@@ -16,13 +16,34 @@ namespace Poker.Controllers
         public BlackJackController(int players = 2)
         {
 
-            for(int i = 0; i < players; i++)
+            for (int i = 0; i < players; i++)
             {
                 //Gives each player a hand, which will be empty
                 Players.Add(new Player());
             }
-            
+
         }
-        
+
+
+        private void EndOfRound()
+        {
+            Payout();
+
+            foreach (var person in Players)
+            {
+                if (person.Money == -50)
+                {
+                    Players.Remove(person);
+                }
+            }
+        }
+        private void Payout()
+        {
+            //Pay out what the players have won/lost this round
+            //Draw: return cost of hand
+            //Win: return 2x cost of hand
+            //Blackjack: Return 3x cost of hand
+            //5 - card Charlie: Return 4x cost of hand
+        }
     }
 }
