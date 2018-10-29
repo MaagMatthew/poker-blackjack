1888f5b9155c77577829bbce08257ffb39637d3f
Matthew Maag <meh.kirby@gmail.com>
2018-10-29 14:44:03 -0600

    Finished the Logic for the Setup Window

--- Poker/Poker/SetUpWindow.xaml.cs
+++ Poker/Poker/SetUpWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,13 +21,15 @@ namespace Poker
     /// </summary>
     public partial class SetUpWindow : Window
     {
-        private int _boundNumber;
+
         public int BoundNumber { get; set; }
+
         public string GameType { get; set; }
         public int Min { get; set; }
         public int Max { get; set; }
         public SetUpWindow(string gt, int min, int max)
         {
+            DataContext = this;
             GameType = gt;
             Min = min;
             Max = max;
@@ -34,25 +38,33 @@ namespace Poker
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-            GameWindow gw = new GameWindow(GameType, 2);
+            GameWindow gw = new GameWindow(GameType, BoundNumber);
             gw.Show();
             this.Close();
         }
 
         private void More_Click(object sender, RoutedEventArgs e)
         {
-            BoundNumber += 1;
+            if(BoundNumber >= Min && BoundNumber < Max)
+            {
+                BoundNumber = BoundNumber + 1;
+                PlayerNum.Text = BoundNumber.ToString();
+            }
         }
 
         private void Fewer_Click(object sender, RoutedEventArgs e)
         {
-            BoundNumber -= 1;
-
+            if (BoundNumber > Min && BoundNumber <= Max)
+            {
+                BoundNumber = BoundNumber - 1;
+                PlayerNum.Text = BoundNumber.ToString();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            BoundNumber = 1;
+            BoundNumber = Min;
+            PlayerNum.Text = BoundNumber.ToString();
         }
     }
 }
